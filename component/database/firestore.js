a7f5038cc95939abb4140427379453d359bfd0cb
Sudarshan R Vittal <sudarshanvittal@gmail.com>
2022-01-26 13:54:03 +0200

    Configured User DB

--- component/database/firestore.js
+++ component/database/firestore.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs, onSnapshot, Setdoc } from 'firebase/firestore';
-import { apiKey, authDomain, databaseURL, projectId, storageBucket, messagingSenderId, appId, measurementId,
+import { getFirestore, collection, getDocs, onSnapshot, addDoc, where, query } from 'firebase/firestore';
+import {
+    apiKey, authDomain, databaseURL, projectId, storageBucket, messagingSenderId, appId, measurementId,
 } from '@env';
 
 const firebaseConfig = {
@@ -18,7 +19,7 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 
-async function getEntryDetails() {                      /**real time updates */
+async function getEntryDetailsRT() {                      /**real time updates */
     const q = collection(db, "EntryDetails");
     const detailsCol = onSnapshot(q, (query) => {
         query.forEach((doc) => {
@@ -28,20 +29,78 @@ async function getEntryDetails() {                      /**real time updates */
     return detailsCol;
 }
 
-async function getEntryDetailsRT() {                                  /**once of update */
+async function getEntryDetails() {                                  /**once of update */
     const detailsCol = collection(db, 'EntryDetails');
     const detailsSnapshot = await getDocs(detailsCol);
     detailsSnapshot.forEach((doc) => {
         console.log(
+            "Date; ", doc.data()["Date"], "\n",
+            "Time; ", doc.data()["Time"], "\n",
             "Name: ", doc.data()["Name"], "\n",
-            "Contact No.: ", doc.data()["Contact No."], "\n",
-            "Vehicle Reg: ", doc.data()["Vehicle Reg"], "\n"
+            "Contact No.: ", doc.data()["Contact_No"], "\n",
+            "Vehicle Reg: ", doc.data()["Vehicle_Reg"], "\n"
         )
     });
     return detailsSnapshot
 }
 
+async function addEntryDetails(date, time, name, conNum, vehReg) {
+    const detailsCol = collection(db, 'EntryDetails');
+    await addDoc(detailsCol, {
+        Date: date,
+        Time: time,
+        Name: name,
+        Contact_No: conNum,
+        Vehicle_Reg: vehReg,
+    })
+}
+
+async function delEntryDetails() {
+    const detailsCol = collection(db, 'EntryDetails');
+    await addDoc(detailsCol, {
+        Date: date,
+        Time: time,
+        Name: name,
+        Contact_No: conNum,
+        Vehicle_Reg: vehReg,
+    })
+}
+
+async function authUser(username, password) {
+    let authName = "";
+    let authPass = "";
+    let type = "";
+    const userCol = collection(db, 'User');
+    const usernameWhere = where("Username", "==", username);
+    const q = query(userCol, usernameWhere);
+    const userDoc = await getDocs(q);
+    userDoc.forEach((doc) => {
+        authName = doc.data()["Username"];
+        authPass = doc.data()["Password"];
+        type = doc.data()["UserType"];
+    });
+    // console.log(
+    //     "DBusername: ",authName,"\n",
+    //     "DBpassword: ",authPass,"\n",
+    //     "DBuserType: ",type,"\n",
+    //     "INusername: ",username,"\n",
+    //     "INpassword: ",password,"\n",
+    // )
+    if (username == authName) {
+        if (password == authPass) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false
+    };
+}
+
 export {
     getEntryDetails,
-    getEntryDetailsRT
+    getEntryDetailsRT,
+    addEntryDetails,
+    delEntryDetails,
+    authUser
 }
